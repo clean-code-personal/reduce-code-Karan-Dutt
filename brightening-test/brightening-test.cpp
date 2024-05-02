3bf87e5ef717986298233ee1473c3e23d84b0d1d
Karan Dutt <karan.dutt@philips.com>
2024-05-02 13:45:06 +0530

    Test code - Duplication removed

--- brightening-test/brightening-test.cpp
+++ brightening-test/brightening-test.cpp
@@ -8,12 +8,33 @@ namespace brighteningtest
 	TEST_CLASS(BrighteningTest)
 	{
 	public:
+
+        struct PixelData
+        {
+            int pixel0;
+            int pixel1;
+            int pixel2;
+            int pixel3;
+        };
+
+        std::shared_ptr<Image> TestSetup(const PixelData& pixelData, const int& rows, const int& columns)
+        {
+            auto image = std::make_shared<Image>(rows, columns);
+			image->pixels[0] = pixelData.pixel0; image->pixels[1] = pixelData.pixel1;
+			image->pixels[2] = pixelData.pixel2; image->pixels[3] = pixelData.pixel3;
+
+            return image;
+        }
+
 		
 		TEST_METHOD(BrightensWholeImage)
 		{
-			auto image = std::make_shared<Image>(2, 2);
-			image->pixels[0] = 45; image->pixels[1] = 55;
-			image->pixels[2] = 65; image->pixels[3] = 254;
+            PixelData pixelData;
+            pixelData.pixel0 = 45;
+            pixelData.pixel1 = 55;
+            pixelData.pixel2 = 65;
+            pixelData.pixel3 = 254;
+			auto image = TestSetup(pixelData, 2, 2);
 
 			ImageBrightener brightener(image);
 			int attenuatedCount = brightener.BrightenWholeImage();
@@ -23,18 +44,24 @@ namespace brighteningtest
 
 		TEST_METHOD(BrightensWithAnotherImage)
 		{
-			auto image = std::make_shared<Image>(2, 2);
-			image->pixels[0] = 45; image->pixels[1] = 55;
-			image->pixels[2] = 65; image->pixels[3] = 75;
+            PixelData pixelData;
+            pixelData.pixel0 = 45;
+            pixelData.pixel1 = 55;
+            pixelData.pixel2 = 65;
+            pixelData.pixel3 = 75;
+			auto image = TestSetup(pixelData, 2, 2);
             ImageBrightener brightener(image);
             
             // Test by brightening only the right part
-            auto brighteningImage = std::make_shared<Image>(2, 2);
-            brighteningImage->pixels[0] = 0; brighteningImage->pixels[1] = 25;
-            brighteningImage->pixels[2] = 0; brighteningImage->pixels[3] = 25;
+            pixelData.pixel0 = 0;
+            pixelData.pixel1 = 25;
+            pixelData.pixel2 = 0;
+            pixelData.pixel1 = 25;
+            auto brighteningImage = TestSetup(pixelData, 2, 2);
+
 
             int attenuatedCount = 0;
-            bool succeeded = brightener.AddBrighteningImage(brighteningImage, attenuatedCount);
+            const bool succeeded = brightener.AddBrighteningImage(brighteningImage, attenuatedCount);
             Assert::IsTrue(succeeded);
             Assert::AreEqual(45, int(image->pixels[0])); // left-side pixel is unchanged
             Assert::AreEqual(80, int(image->pixels[1])); // right-side pixel is brightened
